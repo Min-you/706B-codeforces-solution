ce106a6ebd832b1a4c09ecf12462407e7f129362
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-12 20:26:22 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int cnt=0;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int m;
+    cin>>m;
+    sort(a,a+n);
+    while(m--)
+    {
+        int k, ans;
+        cin>>k;
+        ans=upper_bound(a,a+n, k)-a;
+        cout<<ans<<endl;
+    }
+}
